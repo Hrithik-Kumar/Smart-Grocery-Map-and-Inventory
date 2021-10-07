e2352e75ed9ba55b0b1b138e959982a5e9b3f7fc
r-ho5 <90811303+r-ho5@users.noreply.github.com>
2021-10-07 17:58:19 -0400

    BOUNB-33 changed database to sqlite

--- code/backend/SmartGroceryApp/SmartGroceryApp/settings.py
+++ code/backend/SmartGroceryApp/SmartGroceryApp/settings.py
@@ -82,12 +82,8 @@ MEDIA_URL = '/media/'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'smart_grocery_map',
-        'USER': 'default_user',
-        'PASSWORD': 'default_user',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
