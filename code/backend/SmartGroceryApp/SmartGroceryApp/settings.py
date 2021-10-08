43f7bb8bbe9fa56369df6b67beb0dec77b9d4a09
Hayden M <haydenmlh@gmail.com>
2021-10-08 16:30:57 -0400

    BOUNB-32 Update Django config to include corsheaders
    Update Django config file settings.py to include corsheaders in Installed_Apps

--- code/backend/SmartGroceryApp/SmartGroceryApp/settings.py
+++ code/backend/SmartGroceryApp/SmartGroceryApp/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'companySignUp.apps.CompanysignupConfig',
-    'companylogin.apps.CompanyloginConfig'
+    'companylogin.apps.CompanyloginConfig',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
