3af63c118c94064a421a53c547493605b82c9d47
Nimra <nimra.maqbool@mail.utoronto.ca>
2021-10-08 12:09:24 -0400

    BOUNB-32 Implemented the Login Screen and API

--- code/smart_grocery_map/lib/screens/auth/components/text_input_field.dart
+++ code/smart_grocery_map/lib/screens/auth/components/text_input_field.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:smartgrocery/res/styles.dart';
+import 'package:smart_grocery_map/res/styles.dart';
 
 class TextInputField extends StatelessWidget {
   final TextEditingController controller;
