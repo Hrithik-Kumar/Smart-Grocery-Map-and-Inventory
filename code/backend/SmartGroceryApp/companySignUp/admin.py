c098443b4956f8414fc8333503a9a4f83b399487
changliuroger <60224537+changliuroger@users.noreply.github.com>
2021-10-09 00:04:32 -0400

    Revert "Merge Develop to Main for end of Sprint 1"

--- code/backend/SmartGroceryApp/companySignUp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import company
-from .models import companyProfile
-
-# Register your models here.
-admin.site.register(company)
-admin.site.register(companyProfile)
--- code/backend/SmartGroceryApp/companySignUp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('signup', views.company_sign_up),
-    path('profile', views.company_change_profile)
-]
