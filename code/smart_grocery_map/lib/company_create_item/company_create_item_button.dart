ecf19d567407b1290233b49ca7a7df1d09b843de
r-ho5 <90811303+r-ho5@users.noreply.github.com>
2021-10-17 20:45:27 -0400

    BOUNB-58 base files for company create item

---
+++ code/smart_grocery_map/lib/company_create_item/company_create_item_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Floating Action Button Test',
+      home: CompanyCreateItemFloatingActionButton(),
+    );
+  }
+}
+
+class CompanyCreateItemFloatingActionButton extends StatelessWidget {
+  const CompanyCreateItemFloatingActionButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ code/smart_grocery_map/lib/company_create_item/company_create_item_form.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CompanyCreateItemForm extends StatelessWidget {
+  const CompanyCreateItemForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
