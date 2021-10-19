acb914ee919b1ff026c8df8b5b755d7b90c2e56b
hrui119 <90732898+hrui119@users.noreply.github.com>
2021-10-19 09:57:27 -0400

    BOUNB-46 company login now login to company

--- code/smart_grocery_map/lib/screens/auth/login_screen.dart
+++ code/smart_grocery_map/lib/screens/auth/login_screen.dart
@@ -7,7 +7,7 @@ import '/res/styles.dart';
 import 'components/auth_button.dart';
 import 'package:http/http.dart' as http;
 import 'components/text_input_field.dart';
-import '/screens/user-home/NavBar.dart';
+import '/screens/company-home/NavBar.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
