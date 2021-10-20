eeaf9c66a8a9e4c03e0f03cb8c324938a7565fb0
r-ho5 <90811303+r-ho5@users.noreply.github.com>
2021-10-20 17:07:13 -0400

    BOUNB-58 added floating action button

--- code/smart_grocery_map/lib/company_create_item/company_create_item_button.dart
+++ code/smart_grocery_map/lib/company_create_item/company_create_item_button.dart
@@ -1,34 +1,70 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import './company_create_item_form.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Floating Action Button Test',
-      home: CompanyCreateItemFloatingActionButton(),
-    );
-  }
-}
-
-class CompanyCreateItemFloatingActionButton extends StatelessWidget {
+class CompanyCreateItemFloatingActionButton extends StatefulWidget {
   const CompanyCreateItemFloatingActionButton({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Add your onPressed code here!
-        },
-        child: const Icon(Icons.add),
-      ),
+  CompanyCreateItemFloatingActionButtonState createState() {
+    return CompanyCreateItemFloatingActionButtonState();
+  }
+}
+
+class CompanyCreateItemFloatingActionButtonState
+    extends State<CompanyCreateItemFloatingActionButton> {
+  bool _showButton = true;
+
+  Future<void> _showRemoveCreateButton(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Remove Create Button'),
+          content:
+              const Text('Would you like to remove the create item button?'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Yes'),
+              onPressed: () {
+                setState(() {
+                  _showButton = false;
+                });
+                Navigator.pop(context);
+              },
+            ),
+            TextButton(
+              child: const Text('No'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+          elevation: 24,
+        );
+      },
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_showButton) {
+      return GestureDetector(
+        onLongPress: () => _showRemoveCreateButton(context),
+        child: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const CompanyCreateItemForm(),
+              ),
+            );
+          },
+          child: const Icon(Icons.add),
+        ),
+      );
+    }
+    return Container();
   }
 }
