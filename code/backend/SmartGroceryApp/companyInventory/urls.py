9e8032e7f4d84be6c2d7530bfdb3614f62d7ae18
Hrithik Kumar Advani <hrithik.kumaradvani@mail.utoronto.ca>
2021-10-22 16:45:35 -0400

    BOUNB-63 finished the task of deleting the item from the company inventory

--- code/backend/SmartGroceryApp/companyInventory/urls.py
+++ code/backend/SmartGroceryApp/companyInventory/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('inventory/query', views.company_inventory_query), 
     path('inventory/create', views.company_inventory_create_item),
+    path('inventory/delete', views.company_inventory_delete_item)
 ]
